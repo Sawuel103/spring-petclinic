0d84831df74df506b30c907530ab7ea539167bde
samuel.podhorec <samuel.podhorec@artinsolutions.com>
2020-05-14 12:11:27 +0200

    Initial Commit

---
+++ src/main/java/guru/springframework/petclinic/sfgsppetcinic/SfgSpPetCinicApplication.java
@@ -0,0 +1,13 @@
+package guru.springframework.petclinic.sfgsppetcinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfgSpPetCinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SfgSpPetCinicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/guru/springframework/petclinic/sfgsppetcinic/SfgSpPetCinicApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.springframework.petclinic.sfgsppetcinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfgSpPetCinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
